7e9c05f934eeadb6bffa01b0835ad67de63ef9d1
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-07-02 14:25:48 -0300

    obtendo um usuario por id

--- src/main/java/br/com/portaljmti/workshopmongo/services/UserService.java
+++ src/main/java/br/com/portaljmti/workshopmongo/services/UserService.java
@@ -2,11 +2,13 @@ package br.com.portaljmti.workshopmongo.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.portaljmti.workshopmongo.domain.User;
 import br.com.portaljmti.workshopmongo.repository.UserRepository;
+import br.com.portaljmti.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,15 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+		
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 		
 	}
 
