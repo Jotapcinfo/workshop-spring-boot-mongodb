84953c84a94f08dc2413d1f631681dc7279b2a10
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-07-07 10:47:48 -0300

    Referenciando os posts do usuario

--- src/main/java/br/com/portaljmti/workshopmongo/domain/User.java
+++ src/main/java/br/com/portaljmti/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package br.com.portaljmti.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -15,8 +18,10 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
-	public User() {
-		
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
+	public User() {	
 	}
 
 	public User(String id, String name, String email) {
@@ -50,6 +55,13 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
