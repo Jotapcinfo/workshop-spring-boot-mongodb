d397dd4b1f3edce8f632df04e352e089adb46282
Jefferson Moreno <prowlerurbanhc@gmail.com>
2021-07-07 12:32:32 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/portaljmti/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package br.com.portaljmti.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.portaljmti.workshopmongo.domain.Post;
+import br.com.portaljmti.workshopmongo.repository.PostRepository;
+import br.com.portaljmti.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
